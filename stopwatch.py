13752e1d617dd62a884e115d78666c15cfefb5e7
Gökberk YALTIRAKLI <webdosusb@gmail.com>
2014-01-12 20:53:28 +0200

    Added time formatting.
    It looks like a watch chronometer now.

--- stopwatch.py
+++ stopwatch.py
@@ -2,16 +2,15 @@
 
 import pyglet
 from pyglet.window import key
-from time import time, strftime, localtime
+import time
 
 running = False
 begin, now, total = 0, 0, 0
 size = (320, 240)
-format = "%.2f"
 
-window = pyglet.window.Window(size[0], size[1], caption="Stopwatch", \
+window = pyglet.window.Window(size[0], size[1], caption="Stopwatch", 
                               visible=False)
-label = pyglet.text.Label('0.00', font_size=36,
+label = pyglet.text.Label('00:00:00:00', font_size=36,
                           x=window.width/2, y=window.height/2,
                           anchor_x='center', anchor_y='center')
 window.set_visible()
@@ -22,14 +21,14 @@ def on_key_release(symbol, modifiers):
     if symbol == key.SPACE:
         if not running:
             running = True
-            begin = time()
+            begin = time.time()
         else:
             running = False
-            now = time()
+            now = time.time()
             total += now - begin
     if symbol == key.BACKSPACE:
         total = 0
-        begin = time()
+        begin = time.time()
         running = False
 
 @window.event
@@ -37,13 +36,20 @@ def on_draw():
     window.clear()
     label.draw()
 
+def get_millis(timestamp):
+    millis = str(timestamp - int(timestamp))[2:4]
+    if millis == "":
+        return "00"
+    else:
+        return millis
+
 def update_time(dt):
     global now, begin, total
     if running:
-        now = time()
-        label.text = format % (now - begin + total) 
+        now = time.time()
+        label.text = time.strftime("%H:%M:%S:", time.gmtime(now - begin + total)) + get_millis(now - begin + total)
     else:
-        label.text = format % (total)
+        label.text = time.strftime("%H:%M:%S:", time.gmtime(total)) + get_millis(total)
 
 pyglet.clock.schedule_interval(update_time, 0.01)
 pyglet.app.run()
